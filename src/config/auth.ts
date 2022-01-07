5e1f43f6209676ca7fe22d0cbd7d681f811931b6
Felipe Simmi <simmifelipe1992@gmail.com>
2022-01-07 07:59:40 -0300

    Adicionadas algumas configurações relacionadas ao refresh_token

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  secret_token: "1a64ebcb1de42683bc2f2ba11040bc53",
+  expires_in_token: "15m",
+  secret_refresh_token: "40d0ec852b96c0828892532a7f3aa9fa",
+  expires_in_refresh_token: "30d"
+}
